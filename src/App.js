86684ef3b17457044fd9197bc0c990a57bd77110
mayankpcp7 <jariwalmayank123@gmail.com>
2023-06-27 10:35:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Tabs from './components/Tabs';
+function App() {
+  return (
+    <>
+    <Tabs/>
+    
+    </>
+  );
+}
+
+export default App;
